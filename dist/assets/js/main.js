3e86c492a6151e85a56903a1cd550697dd28185a
udcxx <me@udcxx.me>
2020-10-23 19:29:53 +0900

    worksの作品クリックで大きく表示されるようにした

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,5 +1,5 @@
 // FV Text Animation
-let animationSize = 50;
+let animationSize = 100;
 
 let styles; let i; let randomX; let randomY; let keyframes;
 
@@ -19,7 +19,31 @@ document.addEventListener("DOMContentLoaded", function () {
 			styles.insertRule(keyframes, styles.cssRules.length);
 
 			txts[i].style.left = i - txts.length / 2 + "em";
-			txts[i].style.animation = "move" + i + " 2s 5s both";
+			txts[i].style.animation = "move" + i + " 1.5s 5s both";
 		}
 	}
+});
+
+
+
+// Works
+document.addEventListener("DOMContentLoaded", function () {
+	const btns = document.querySelectorAll('.list-content img');
+	btns.forEach((btn) => {
+		btn.addEventListener('click', function() {
+			document.getElementById('show_box').style.opacity = 0;
+			const h4 = this.dataset.h4;
+			const h3 = this.dataset.h3;
+			const p = this.dataset.p;
+			window.setTimeout(function() {
+				document.getElementById('show_ttl_sub').textContent = h4;
+				document.getElementById('show_ttl').textContent = h3;
+				document.getElementById('show_txt').textContent = p;
+			}, 500);
+			window.setTimeout(function() {
+				document.getElementById('show_box').style.opacity = 1;
+			}, 550);
+
+		});
+	});
 });
