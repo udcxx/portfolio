7327e70d0d16cde0f9d21ed732ba78cfc1784094
udcxx <me@udcxx.me>
2021-01-14 23:05:15 +0900

    gitignore書き換え

--- src/recaptcha_vars.php
+++
@@ -1,7 +0,0 @@
-<?php
-// reCAPTCHA v2 サイトキー
-define('V2_SITEKEY', '6LcH4ysaAAAAAHmGu1JV_yyidJ2eynfx0kvWvG4c');
-
-// reCAPTCHA v2 シークレットキー
-define('V2_SECRETKEY', '6LcH4ysaAAAAAJpp8rnNcUc15nAJLVUOFMmcCpyy');
-?>
