960548dc1f52d1f3a02bebafe394c02f7584cbd9
udcxx <me@udcxx.me>
2024-06-25 15:56:13 +0900

    最新のブログ記事を表示できるように

---
+++ components/atoms/GetBlogArticles.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const { data } = await useFetch('https://app.udcxx.me/api/blog/?type=article&limit=3&order=DESC');
+    const response = JSON.parse(data._rawValue);
+</script>
+
+<template>
+    <ul>
+        <li>
+            <a :href="response.articles[0].url">{{ response.articles[0].eyecatchemoji }} {{ response.articles[0].title }}</a>
+        </li>
+        <li>
+            <a :href="response.articles[1].url">{{ response.articles[1].eyecatchemoji }} {{ response.articles[1].title }}</a>
+        </li>
+        <li>
+            <a :href="response.articles[2].url">{{ response.articles[2].eyecatchemoji }} {{ response.articles[2].title }}</a>
+        </li>
+    </ul>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,11 @@
         <h2>udcxx.</h2>
     </section><!-- /fv -->
 
-    <ToppageSection titleJa="udcxxについて" titleEn="about">
+    <ToppageSection title-ja="udcxxについて" title-en="about">
         <AtomsBord>こんにちは👋<br>ポートフォリオサイトをご覧いただき、ありがとうございます🙌</AtomsBord>
     </ToppageSection><!-- /about -->
 
-    <ToppageSection titleJa="制作実績" titleEn="creative">
+    <ToppageSection title-ja="制作実績" titleEn="creative">
         <CreativeCard href="https://app.udcxx.me/recitone/" title="RECITONE" subtitle="AIでkintone活用をサポート！" eyecatch="/creative/recitone.png" :isBig="true">
             <p>AIを使ったWebツール「RECITONE」を開発しました。<br>RECITONEは、kintoneで何を管理したいかを入力するだけで、kintoneアプリの設計図となる「レシピ」をAIが提案してくれるツールです。</p>
         </CreativeCard>
@@ -17,7 +17,13 @@
 
     </ToppageSection><!-- /creative -->
 
-    <ToppageSection titleJa="これまでの経歴" titleEn="history">
+    <ToppageSection title-ja="読んだもの書いたもの" title-en="activity">
+        <AtomsBord>
+            <AtomsGetBlogArticles />
+        </AtomsBord>
+    </ToppageSection>
+
+    <ToppageSection title-ja="これまでの経歴" title-en="history">
 
     </ToppageSection><!-- /history -->
 </template>
