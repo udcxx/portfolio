184e4fc911459ceaf326aff81cabaa9bbff13cf0
udcxx <me@udcxx.me>
2024-07-16 14:08:38 +0900

    nuxt update to v3.12.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	future: {
+		compatibilityVersion: 4,
+	},
 	app: {
 		baseURL: '/portfolio/', // Dev環境用
 		head: {
