8ce27df79d7c41c233cf69393106913b1b8055bf
udcxx <me@udcxx.me>
2024-07-17 15:36:20 +0900

    スムーススクロールとヘッダー非表示の競合解消

--- app/components/atoms/TextLink.vue
+++ app/components/atoms/TextLink.vue
@@ -4,8 +4,6 @@
         type: string
     }
     const props: any= defineProps<Props>();
-    
-    let linkToType: 'inPage'|'outPage' = props.href.indexOf('#') === 0 ? 'inPage' : 'outPage';
 
     const inPageScroll = (href: string) => {
         setSmoothScroll(href);
@@ -20,14 +18,30 @@
         window.scroll({
             top: top,
             behavior: "smooth"
-        })
+        });
+
+        // ヘッダー非表示
+        const headerEl: any= document.querySelector('.header_bar');
+        if (headerEl && headerEl.classList.contains('up')) {
+            headerEl.classList.remove('up');
+            headerEl.classList.add('down');
+            setTimeout(() => {
+                headerEl.style.opacity = 0;
+            }, 500);
+        }
+
+        setTimeout(() => {
+            headerEl.classList.remove('up');
+            headerEl.classList.add('down');
+            headerEl.style.opacity = 1;
+        }, 1000);
     };
 </script>
 
 <template>
-    <NuxtLink v-if="linkToType = 'outPage'" :to="props.href" :class="props.type"><slot /></NuxtLink>
+    <a v-if="props.href.indexOf('#') === 0" :class="props.type" @click="inPageScroll(props.href)"><slot /></a>
 
-    <a v-else-if="linkToType = 'inPage'" :class="props.type" @click="inPageScroll(props.href)"><slot /></a>
+    <NuxtLink v-else :to="props.href" :class="props.type" @click="console.log(props.href.indexOf('#'))"><slot /></NuxtLink>
 </template>
 
 <style lang="scss" scoped>
